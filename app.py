9cf81ca141b3e4e684de7920cc5e36194886c502
Sai <sai@example.com>
2026-07-06 19:40:52 +0530

    fileadded

---
+++ app.py
@@ -0,0 +1,11 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(("127.0.0.1", 8080))
+
+message = client.recv(1024)
+
+print(message.decode())
+
+client.close()
